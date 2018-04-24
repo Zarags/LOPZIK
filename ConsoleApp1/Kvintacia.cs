b2e8960326bd6407b3113fefae8fcf4b376bd51d
Zarags <nikitapirozkov9@gmail.com>
2018-04-24 11:47:22 +0300

    git commit mhjgmhg git commit -m

---
+++ ConsoleApp1/Kvintacia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Kvintacia
+    {
+        public string Nomer { get; set; }
+        public string Data { get; set; }
+        public float Syma { get; set; }
+   
+        public Kvintacia()
+        {
+        }
+       
+        public void Input()
+        {
+            Nomer = Console.ReadLine();
+            Data = Console.ReadLine();
+            Syma = float.Parse(Console.ReadLine());
+        }
+    }
+}
+    
+
